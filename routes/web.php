206cd911d22dfb71be030788a17202eb1eeb6a7f
Nattawut Wai <wardnep@gmail.com>
2026-04-07 15:34:45 +0700

    fixed data

--- routes/web.php
+++ routes/web.php
@@ -7,19 +7,31 @@ use App\Http\Controllers\Journey\HomeController;
 Route::get('/', function () {
     // dd('https://wn.in.th');
 
-    foreach (App\Models\JourneyItem::whereIn('journey_id', [9, 10, 11, 12])->get() as $item1) {
-        $item2 = App\Models\JourneyItem2::find($item1->id);
-        if ($item2) {
-            $item1->strategy = $item2->strategy;
-            $item1->save();
-
-            echo $item1->strategy + " " + $item2->strategy."<br />";
-        } else {
-            echo $item1->id + " not found<br />";
-        }
+    App\Models\JourneyItem::truncate();
+    $journey_item2s = App\Models\JourneyItem2::all();
+    foreach ($journey_item2s as $journey_item2) {
+        $journey_item = new App\Models\JourneyItem;
+        $journey_item->journey_id = $journey_item2->journey_id ?: '';
+        $journey_item->date = $journey_item2->date ?: '';
+        $journey_item->entry_session = $journey_item2->entry_session ?: '';
+        $journey_item->exit_session = $journey_item2->exit_session ?: '';
+        $journey_item->position = $journey_item2->position ?: '';
+        $journey_item->result = $journey_item2->result ?: '';
+        $journey_item->size = $journey_item2->size ?: '';
+        $journey_item->tp1 = $journey_item2->tp1 ?: '';
+        $journey_item->tp2 = $journey_item2->tp2 ?: '';
+        $journey_item->result_r1 = $journey_item2->result_r1 ?: '';
+        $journey_item->result_r2 = $journey_item2->result_r2 ?: '';
+        $journey_item->result_r2 = $journey_item2->result_r2 ?: '';
+        $journey_item->strategy = $journey_item2->strategy ?: '';
+        $journey_item->grade = $journey_item2->grade ?: '';
+        $journey_item->image = $journey_item2->image ?: '';
+        $journey_item->image2 = $journey_item2->image2 ?: '';
+        $journey_item->note = $journey_item2->note ?: '';
+        $journey_item->save();
     }
 
-    dd('Done!');
+    dd('done');
 });
 
 Route::get('journey/summary_tp', [HomeController::class, 'summary_tp']);
