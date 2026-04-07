73e69c240d5b8123116c97e3e5c38ff0509b031b
Nattawut Wai <wardnep@gmail.com>
2026-04-07 12:46:19 +0700

    adminlte route controler resourse created

--- resources/views/journey/home.blade.php
+++ resources/views/journey/home.blade.php
@@ -1,1 +1,11 @@
 @extends('_layouts.journey')
+
+@section('title', 'WN Journey')
+
+@section('content_header')
+    new content header
+@endsection
+
+@section('content')
+    new content
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use App\Http\Controllers\Journey\HomeController;
 
 Route::get('/', function () {
-    return view('journey.home');
+    dd('https://wn.in.th');
 });
+
+Route::get('journey', [HomeController::class, 'index']);
