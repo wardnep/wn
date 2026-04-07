40b038001998768edd1aff598a70ee61e7ad82cb
Nattawut Wai <wardnep@gmail.com>
2026-04-07 15:16:46 +0700

    fixed data

---
+++ app/Models/JourneyItem2.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class JourneyItem2 extends Model
+{
+    use HasFactory;
+
+    protected $connection = 'mysql_second';
+    protected $table = 'journey_items';
+}
