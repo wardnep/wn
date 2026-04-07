3750c311d2ef0e095a4100c0798c4d0701674ee9
Nattawut Wai <wardnep@gmail.com>
2026-04-07 16:36:05 +0700

    login - enable register

--- routes/web.php
+++ routes/web.php
@@ -27,4 +27,4 @@ Route::middleware(['auth'])->group(function () {
     Route::post('journey', [HomeController::class, 'storeOrUpdate']);
 });
 
-Auth::routes(['register' => false]);
+Auth::routes(['register' => true]);
