d79c961484c5e9f354b9d6e0bc88c886e8161361
Nattawut Wai <wardnep@gmail.com>
2026-04-24 15:23:03 +0700

    update index page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,5 +2,5 @@
     <a href="{{ url('journey') }}">Trading Journey</a><br />
     <a href="{{ url('quant') }}">Algorithmic Trading </a><br />
     <a href="{{ url('sql') }}">SQL Query</a><br />
-    <a href="https://github.com/wardnep">Github</a>
+    <a target="_blank" href="https://github.com/wardnep">Github</a>
 </div>
