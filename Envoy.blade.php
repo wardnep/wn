239a2ab2af7fdd958e7fa0be7ece9d8867d58339
Nattawut Wai <wardnep@gmail.com>
2026-04-28 16:15:36 +0700

    adminer setup

--- Envoy.blade.php
+++ Envoy.blade.php
@@ -10,7 +10,7 @@
 	ln -s ../env .env
 	rm -rf storage
 	ln -s ../storage storage
-	ln -s ../adminer adminer
+	ln -s ../../adminer public/adminer
 	php artisan migrate --no-interaction
 	php artisan optimize
 	php artisan config:clear
