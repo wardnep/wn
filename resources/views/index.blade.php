d845dafd4865766c42c489bfe944d31bf6226279
Nattawut Wai <wardnep@gmail.com>
2026-04-30 16:59:35 +0700

    fixed adminer link

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,6 +1,6 @@
 <div align="center">
     <a href="{{ url('journey') }}">Trading Journey</a><br />
     <a href="{{ url('quant') }}">Algorithmic Trading </a><br />
-    <a href="{{ url('http://wn.th/adminer/sqlite.php') }}">Adminer</a><br /><br />
+    <a href="{{ url('http://wn.in.th/adminer/sqlite.php') }}">Adminer</a><br /><br />
     <a target="_blank" href="https://github.com/wardnep">Github</a>
 </div>
