a443d40b76a55208a3124a52cc3d213471d7e4b7
Nattawut Wai <wardnep@gmail.com>
2026-05-08 16:14:40 +0700

    update paginate

--- resources/views/journey/index14.blade.php
+++ resources/views/journey/index14.blade.php
@@ -46,7 +46,6 @@
                             $select_journey_id = $select_journey ? $select_journey->id : 0;
                             $edit_journey_item_id = $edit_journey_item ? $edit_journey_item->id : 0;
                         @endphp
-                         {{ $journey_items->currentPage() }} / {{ $journey_items->lastPage() }}
                         <table class="table table-bordered table-striped">
                             <tbody>
                                 <tr>
