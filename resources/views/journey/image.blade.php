ba9847868a67cc6d3f760bd31ee9c2481d2624d3
Nattawut Wai <wardnep@gmail.com>
2026-05-14 09:30:14 +0700

    update image page

--- resources/views/journey/image.blade.php
+++ resources/views/journey/image.blade.php
@@ -15,6 +15,13 @@
             </td>
         </tr>
         <tr>
+            <td></td>
+            <td colspan="2" align="center">
+                <font color="#FFFFFF">{{ $item->note }}</font>
+            </td>
+            <td></td>
+        </tr>
+        <tr>
             <td width="2%">
                 @if ($prev_item)
                     <a style="color:white" href="{{ url('journey/image/'.$prev_item->journey_id.'/'.$prev_item->id) }}">prev</a>
@@ -37,13 +44,6 @@
                     <a style="color:white" href="{{ url('journey/image/'.$next_item->journey_id.'/'.$next_item->id) }}">next</a>
                 @endif
             </td>
-        </tr>
-        <tr>
-            <td></td>
-            <td colspan="2" align="center">
-                <font color="#FFFFFF">{{ $item->note }}</font>
-            </td>
-            <td></td>
         </tr>
     </table>
 </body>
