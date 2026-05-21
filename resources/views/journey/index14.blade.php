7f64e8b7f9d3775867f46b236b7814f03dceb308
Nattawut Wai <wardnep@gmail.com>
2026-05-21 15:17:00 +0700

    add last-page paginate

--- resources/views/journey/index14.blade.php
+++ resources/views/journey/index14.blade.php
@@ -98,6 +98,7 @@
                                                 'select_journey_id' => $select_journey ? $select_journey->id : 0
                                             ])->links() }}
                                         </div>
+                                        <br />
                                         <a href="https://wn.in.th/journey?select_journey_id={{ $select_journey->id }}&amp;page={{ $lastPage }}" rel="next" class="relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 leading-5 rounded-md hover:text-gray-500 focus:outline-none focus:ring ring-gray-300 focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150">
                                             Last »
                                         </a>
