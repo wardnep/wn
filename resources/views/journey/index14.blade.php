699d04b6a2e20f5e85281ba31cf908e10af682af
Nattawut Wai <wardnep@gmail.com>
2026-05-21 17:31:14 +0700

    fixed lastpage

--- resources/views/journey/index14.blade.php
+++ resources/views/journey/index14.blade.php
@@ -99,7 +99,7 @@
                                             ])->links() }}
                                         </div>
                                         <br />
-                                        @if (isset($page) && $page !== $last_page)
+                                        @if (isset($page) && $page < $last_page)
                                             <a href="https://wn.in.th/journey?select_journey_id={{ $select_journey->id }}&amp;page={{ $last_page }}" rel="next" class="relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 leading-5 rounded-md hover:text-gray-500 focus:outline-none focus:ring ring-gray-300 focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150">
                                                 Last »
                                             </a>
