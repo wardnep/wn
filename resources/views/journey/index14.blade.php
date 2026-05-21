09c663a0621403fceb1cea0bb1f73c7885d6bbb5
Nattawut Wai <wardnep@gmail.com>
2026-05-21 17:29:02 +0700

    fixed lastpage

--- resources/views/journey/index14.blade.php
+++ resources/views/journey/index14.blade.php
@@ -99,9 +99,11 @@
                                             ])->links() }}
                                         </div>
                                         <br />
-                                        <a href="https://wn.in.th/journey?select_journey_id={{ $select_journey->id }}&amp;page={{ $lastPage }}" rel="next" class="relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 leading-5 rounded-md hover:text-gray-500 focus:outline-none focus:ring ring-gray-300 focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150">
-                                            Last »
-                                        </a>
+                                        @if (isset($page) && $page !== $last_page)
+                                            <a href="https://wn.in.th/journey?select_journey_id={{ $select_journey->id }}&amp;page={{ $last_page }}" rel="next" class="relative inline-flex items-center px-4 py-2 text-sm font-medium text-gray-700 bg-white border border-gray-300 leading-5 rounded-md hover:text-gray-500 focus:outline-none focus:ring ring-gray-300 focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150">
+                                                Last »
+                                            </a>
+                                        @endif
                                     </td>
                                 </tr>
                                 <form method="POST" action="{{ url('journey') }}" enctype="multipart/form-data">
