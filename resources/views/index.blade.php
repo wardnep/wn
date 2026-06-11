91e677c71726c1a567ed61f01d5c4d62467bdeda
Nattawut Wai <wardnep@gmail.com>
2026-06-11 10:29:44 +0700

    update icon

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,12 +1,17 @@
 <html>
 <head>
     <title>wn</title>
+    <link rel="stylesheet" href="http://localhost:8000/vendor/fontawesome-free/css/all.min.css">
+    <link rel="stylesheet" href="http://localhost:8000/vendor/overlayScrollbars/css/OverlayScrollbars.min.css">
+    <link rel="stylesheet" href="http://localhost:8000/vendor/adminlte/dist/css/adminlte.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 <body>
     <div align="center">
-        📝 <a href="{{ url('journey') }}">Trading Journey</a><br />
-        🤖 <a href="{{ url('quant') }}">Algorithmic Trading</a><br />
-        🚨 <a href="{{ url('alert') }}">Price Alert</a><br /><br />
-        🚀 <a target="_blank" href="https://github.com/wardnep">Github</a>
+        <br /><br />
+        <i class="fa fa-book" aria-hidden="true"></i> <a href="{{ url('journey') }}">Trading Journey</a><br />
+        <i class="fa fa-cogs" aria-hidden="true"></i> <a href="{{ url('quant') }}">Algorithmic Trading</a><br />
+        <i class="fa fa-bell" aria-hidden="true"></i> <a href="{{ url('alert') }}">Price Alert</a><br /><br />
+        <i class="fa fa-rocket" aria-hidden="true"></i> <a target="_blank" href="https://github.com/wardnep">Github</a>
     </div>
     <!-- Google tag (gtag.js) -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-LPBRPKTYDE"></script>
