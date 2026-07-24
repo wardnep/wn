fa4e8117e15f202f28251400e63306a729ab3569
Nattawut Wai <wardnep@gmail.com>
2026-07-24 10:37:44 +0700

    create access-ip log, position-sizing page only

--- app/Http/Controllers/PositionSizingController.php
+++ app/Http/Controllers/PositionSizingController.php
@@ -4,10 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\AccessIp;
+
 class PositionSizingController extends Controller
 {
     public function index()
     {
+        $access_ip = new AccessIp();
+        $access_ip->ip = $request->header('CF-Connecting-IP') ?? $request->ip();
+        $access_ip->save();
+
         return view('position_sizing.index');
     }
 }
