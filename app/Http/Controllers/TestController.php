63b5e8ef186c3895699273d9a10a25ccd0e6f00c
Nattawut Wai <wardnep@gmail.com>
2026-08-06 09:56:01 +0700

    test Controller update

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -12,21 +12,21 @@ class TestController extends Controller
 {
     public function index(IpWhoisService $whois)
     {
-        foreach (AccessIp::all() as $access_ip) {
+        dd(AccessIp::whereIsNull('isp')->get());
+
+        foreach (AccessIp::where('isp', '=', '')->get() as $access_ip) {
             $result = $whois->lookup($access_ip->ip);
 
-            dd($result);
+            // dd($result);
 
-            if (!$response->successful()) {
-                dd($response->status());
+            if (!$result) {
+                // dd($response->status());
             } else {
-                $data = $response->json();
-
-                $access_ip->isp = $data['isp'];
-                $access_ip->org = $data['org'];
-                $access_ip->country = $data['country'];
-                $access_ip->city = $data['city'];
-                $access_ip->whois_raw = json_encode($data);
+                $access_ip->isp = $result['isp'];
+                $access_ip->org = $result['org'];
+                $access_ip->country = $result['country'];
+                $access_ip->city = $result['city'];
+                $access_ip->whois_raw = json_encode($result);
                 $access_ip->save();
             }
         }
